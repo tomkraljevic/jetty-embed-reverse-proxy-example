fb3e5ec19c8894983697e20c1baaeca5f9f8b8a5
Tom Kraljevic <tomk@tomk.net>
2017-05-01 20:03:21 -0700

    Make it https. Remove dead code.

--- src/main/java/org/eclipse/jetty/embedded/ProxyServer.java
+++ src/main/java/org/eclipse/jetty/embedded/ProxyServer.java
@@ -18,92 +18,16 @@
 
 package org.eclipse.jetty.embedded;
 
-//import org.eclipse.jetty.servlets.AsyncProxyServlet;
 import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.server.handler.ConnectHandler;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlets.ProxyServlet;
 
-//import org.eclipse.jetty.server.nio.SelectChannelConnector;
-
 public class ProxyServer
 {
-//  public static void main( String[] args ) throws Exception
-//  {
-//    Server server = new Server();
-//    ServerConnector connector = new ServerConnector(server);
-//    connector.setPort(8888);
-//    server.addConnector(connector);
-//
-//    // Setup proxy handler to handle CONNECT methods
-//    ConnectHandler proxy = new ConnectHandler();
-//    server.setHandler(proxy);
-//
-//    // Setup proxy servlet
-//    ServletContextHandler context = new ServletContextHandler(proxy, "/",
-//            ServletContextHandler.SESSIONS);
-//    ServletHolder proxyServlet = new ServletHolder(ProxyServlet.class);
-//    proxyServlet.setInitParameter("blackList", "www.eclipse.org");
-//    context.addServlet(proxyServlet, "/*");
-//
-//    server.start();
-//  }
-
-  /*
-  public static void main(String[] args) throws Exception {
-    Server server=new Server();
-//    SelectChannelConnector selectChannelConnector=new SelectChannelConnector();
-//    server.setConnectors(new Connector[]{selectChannelConnector});
-//    selectChannelConnector.setPort(8080);
-
-//    ContextHandler.Context servletContext=new ServletContextHandler.Context(Context.NO_SECURITY | Context.NO_SESSIONS);
-
-    ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.NO_SECURITY | ServletContextHandler.NO_SESSIONS);
-
-
-//    server.setHandler(servletContext);
-    ServletHandler servletHandler=contextHandler.getServletHandler();
-    ServletHolder proxy=new ServletHolder(ProxyServlet.Transparent.class);
-    servletHandler.addServletWithMapping(proxy,"/ws/*");
-    proxy.setInitParameter("ProxyTo","http://www.webtide.com");
-    proxy.setInitParameter("Prefix","/ws");
-//    FilterHolder filter=servletHandler.addFilterWithMapping(ContinuationFilter.class,"/*",0);
-//    filter.setInitParameter("debug","true");
-    server.start();
-    server.join();
-  }
-  */
-
-//  public static void main(String[] args) throws Exception {
-//    Server server = new Server();
-//    ServerConnector connector = new ServerConnector(server);
-//    connector.setPort(8888);
-//    server.addConnector(connector);
-//
-////    AsyncProxyServlet.Transparent proxy = new AsyncProxyServlet.Transparent();
-//
-//    ServletHandler servletHandler=contextHandler.getServletHandler();
-//    ServletHolder proxy=new ServletHolder(AsyncProxyServlet.Transparent.class);
-//    proxy.setInitParameter("ProxyTo","http://www.webtide.com");
-//    proxy.setInitParameter("Prefix","/ws");
-//
-//  //  ConnectHandler handler = new ConnectHandler();
-//
-//    server.setHandler(proxy);
-//
-//    // Setup proxy handler to handle CONNECT methods
-////    ConnectHandler proxy = new ConnectHandler();
-////    server.setHandler(proxy);
-//  }
-
-
-
   private static void reverseProxy() throws Exception{
     Server server = new Server();
 
@@ -120,7 +44,7 @@ public class ProxyServer
     // Setup proxy servlet
     ServletContextHandler context = new ServletContextHandler(proxy, "/", ServletContextHandler.SESSIONS);
     ServletHolder proxyServlet = new ServletHolder(ProxyServlet.Transparent.class);
-    proxyServlet.setInitParameter("ProxyTo", "http://localhost:54321/");
+    proxyServlet.setInitParameter("ProxyTo", "https://localhost:54321/");
     proxyServlet.setInitParameter("Prefix", "/");
     context.addServlet(proxyServlet, "/*");
 
